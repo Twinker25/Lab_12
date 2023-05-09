5e40ba2b830ee1a0c21a48ae97aacbd09b7979df
Twinker25 <shashokd@gmail.com>
2023-05-09 19:41:39 +0300

    Add Task 1 - 5

--- Lab_12/Program.cs
+++ Lab_12/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml.Serialization;
+﻿using System;
+using System.Xml.Serialization;
 
 namespace Lab_12
 {
@@ -6,16 +7,129 @@ namespace Lab_12
     {
         static void Main(string[] args)
         {
-            Calculator<double> calculator = new Calculator<double>();
-
-            Console.Write("Enter number 1: ");
-            int num1 = int.Parse(Console.ReadLine());
-            Console.Write("Enter number 2: ");
-            int num2 = int.Parse(Console.ReadLine());
-            Console.WriteLine($"{num1} + {num2} = {calculator.Add(num1, num2)}");
-            Console.WriteLine($"{num1} - {num2} = {calculator.Sub(num1, num2)}");
-            Console.WriteLine($"{num1} * {num2} = {calculator.Mult(num1, num2)}");
-            Console.WriteLine($"{num1} / {num2} = {calculator.Div(num1, num2)}");
+            int choice, n1, n2, n3, number;
+            do
+            {
+                Console.Write("Enter task (1 - 5) or Calculator (6): ");
+                choice = int.Parse(Console.ReadLine());
+                switch (choice) 
+                {
+                    case 1:
+                        Task1_2<double> max = new Task1_2<double>();
+                        Console.Write("Enter number 1: ");
+                        n1 = int.Parse(Console.ReadLine());
+                        Console.Write("Enter number 2: ");
+                        n2 = int.Parse(Console.ReadLine());
+                        Console.Write("Enter number 3: ");
+                        n3 = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"Max number: {max.Max(n1, n2, n3)}");
+                        break;
+                    case 2:
+                        Task1_2<double> min = new Task1_2<double>();
+                        Console.Write("Enter number 1: ");
+                        n1 = int.Parse(Console.ReadLine());
+                        Console.Write("Enter number 2: ");
+                        n2 = int.Parse(Console.ReadLine());
+                        Console.Write("Enter number 3: ");
+                        n3 = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"Min number: {min.Min(n1, n2, n3)}");
+                        break;
+                    case 3:
+                        Task3<int> taskInt = new Task3<int>();
+                        Console.Write("Enter size of array: ");
+                        int size = int.Parse(Console.ReadLine());
+                        int[] array = new int[size];
+                        Random rand = new Random();
+                        for(int i = 0; i < array.Length; i++)
+                        {
+                            array[i] = rand.Next(-9, 10);
+                            Console.Write(array[i] + " ");
+                        }
+                        int sum = taskInt.SumArray(array);
+                        Console.WriteLine("\nSum of element array: " + sum);
+                        break;
+                    case 4:
+                        Stack<int> stack = new Stack<int>();
+                        while (true)
+                        {
+                            Console.Write("\n1 - Push\n2 - Pop\n3 - Peek\n4 - Count\n0 - Exit\nEnter choice: ");
+                            int ch = int.Parse(Console.ReadLine());
+                            switch (ch)
+                            {
+                                case 1:
+                                    Console.Write("Enter number: ");
+                                    number = int.Parse(Console.ReadLine());
+                                    stack.Push(number);
+                                    break;
+                                case 2:
+                                    stack.Pop();
+                                    break;
+                                case 3:
+                                    int peek = stack.Peek();
+                                    Console.WriteLine("Peek of stack: " + peek);
+                                    break;
+                                case 4:
+                                    int count = stack.Count;
+                                    Console.WriteLine("Count of stack: " + count);
+                                    break;
+                                case 0:
+                                    Environment.Exit(0);
+                                    break;
+                                default:
+                                    Console.WriteLine("Error!");
+                                    break;
+                            }
+                        }
+                        break;
+                    case 5:
+                        Queue<int> queue = new Queue<int>();
+                        while (true)
+                        {
+                            Console.Write("\n1 - Enqueue\n2 - Dequeue\n3 - Peek\n4 - Count\n0 - Exit\nEnter choice: ");
+                            int ch = int.Parse(Console.ReadLine());
+                            switch (ch)
+                            {
+                                case 1:
+                                    Console.Write("Enter number: ");
+                                    number = int.Parse(Console.ReadLine());
+                                    queue.Enqueue(number);
+                                    break;
+                                case 2:
+                                    queue.Dequeue();
+                                    break;
+                                case 3:
+                                    int peek = queue.Peek();
+                                    Console.WriteLine("Peek of stack: " + peek);
+                                    break;
+                                case 4:
+                                    int count = queue.Count;
+                                    Console.WriteLine("Count of stack: " + count);
+                                    break;
+                                case 0:
+                                    Environment.Exit(0);
+                                    break;
+                                default:
+                                    Console.WriteLine("Error!");
+                                    break;
+                            }
+                        }
+                        break;
+                    case 6:
+                        Calculator<double> calculator = new Calculator<double>();
+                        Console.Write("Enter number 1: ");
+                        int num1 = int.Parse(Console.ReadLine());
+                        Console.Write("Enter number 2: ");
+                        int num2 = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"{num1} + {num2} = {calculator.Add(num1, num2)}");
+                        Console.WriteLine($"{num1} - {num2} = {calculator.Sub(num1, num2)}");
+                        Console.WriteLine($"{num1} * {num2} = {calculator.Mult(num1, num2)}");
+                        Console.WriteLine($"{num1} / {num2} = {calculator.Div(num1, num2)}");
+                        break;
+                    default:
+                        Console.WriteLine("Error!");
+                        break;
+                }
+            } while (choice < 1 || choice > 5);
         }
     }
 }
---
+++ Lab_12/Task.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_12
+{
+    public class Task1_2<T>
+    {
+        public T Max(T a, T b, T c)
+        {
+            dynamic n1 = (dynamic)a;
+            dynamic n2 = (dynamic)b;
+            dynamic n3 = (dynamic)c;
+            if (n1 >= n2 && n1 >= n3)
+            {
+                return n1;
+            }
+            else if (n2 >= n1 && n2 >= n3)
+            {
+                return n2;
+            }
+            else
+            {
+                return n3;
+            }
+        }
+        public T Min(T a, T b, T c)
+        {
+            dynamic n1 = (dynamic)a;
+            dynamic n2 = (dynamic)b;
+            dynamic n3 = (dynamic)c;
+            if (n1 <= n2 && n1 <= n3)
+            {
+                return n1;
+            }
+            else if (n2 <= n1 && n2 <= n3)
+            {
+                return n2;
+            }
+            else
+            {
+                return n3;
+            }
+        }
+    }
+
+
+
+    public class Task3<T>
+    {
+        public T SumArray(T[] array)
+        {
+            dynamic sum = default(T);
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += (dynamic)array[i];
+            }
+            return sum;
+        }
+    }
+
+
+
+    public class Stack<T>
+    {
+        private List<T> element;
+
+        public Stack()
+        {
+            element = new List<T>();
+        }
+
+        public void Push(T item)
+        {
+            element.Add(item);
+        }
+
+        public T Pop()
+        {
+            if (element.Count == 0)
+            {
+                Console.WriteLine("The stack is empty");
+                Environment.Exit(0);
+            }
+
+            T item = element[element.Count - 1];
+            element.RemoveAt(element.Count - 1);
+            return item;
+        }
+
+        public T Peek()
+        {
+            if (element.Count == 0)
+            {
+                Console.WriteLine("The stack is empty");
+                Environment.Exit(0);
+            }
+
+            return element[element.Count - 1];
+        }
+
+        public int Count
+        {
+            get { return element.Count; }
+        }
+    }
+
+
+
+    public class Queue<T>
+    {
+        private LinkedList<T> elements;
+
+        public Queue()
+        {
+            elements = new LinkedList<T>();
+        }
+
+        public void Enqueue(T item)
+        {
+            elements.AddLast(item);
+        }
+
+        public T Dequeue()
+        {
+            if (elements.Count == 0)
+            {
+                Console.WriteLine("The queue is empty");
+                Environment.Exit(0);
+            }
+
+            T item = elements.First.Value;
+            elements.RemoveFirst();
+            return item;
+        }
+
+        public T Peek()
+        {
+            if (elements.Count == 0)
+            {
+                Console.WriteLine("The queue is empty");
+                Environment.Exit(0);
+            }
+
+            return elements.First.Value;
+        }
+
+        public int Count
+        {
+            get { return elements.Count; }
+        }
+    }
+}
