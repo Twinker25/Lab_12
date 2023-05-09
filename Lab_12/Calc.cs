0a40463d6ccb26ace4bc4d780a23b228beb7f8f7
Twinker25 <shashokd@gmail.com>
2023-05-09 18:32:44 +0300

    Add project files.

---
+++ Lab_12/Calc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_12
+{
+    internal class Calculator<T>
+    {
+        public T Add(T first, T second)
+        {
+            dynamic a = (dynamic)first; 
+            dynamic b = (dynamic)second; 
+            return a + b;
+        }
+        public T Sub(T first, T second)
+        {
+            dynamic a = (dynamic)first;
+            dynamic b = (dynamic)second;
+            return a - b;
+        }
+        public T Mult(T first, T second)
+        {
+            dynamic a = (dynamic)first;
+            dynamic b = (dynamic)second;
+            return a * b;
+        }
+        public T Div(T first, T second)
+        {
+            dynamic a = (dynamic)first;
+            dynamic b = (dynamic)second;
+            return a / b;
+        }
+    }
+}
---
+++ Lab_12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Serialization;
+
+namespace Lab_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator<double> calculator = new Calculator<double>();
+
+            Console.Write("Enter number 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter number 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{num1} + {num2} = {calculator.Add(num1, num2)}");
+            Console.WriteLine($"{num1} - {num2} = {calculator.Sub(num1, num2)}");
+            Console.WriteLine($"{num1} * {num2} = {calculator.Mult(num1, num2)}");
+            Console.WriteLine($"{num1} / {num2} = {calculator.Div(num1, num2)}");
+        }
+    }
+}
